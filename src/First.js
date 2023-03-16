54d4043d00aad4ae6e3c6dc62af045e290ff8e9c
Adrian <apparenelle@gmail.com>
2023-03-16 15:17:17 -0400

    media query bug fix
    got the media query bug fixed needed meta data

--- src/First.js
+++ src/First.js
@@ -12,7 +12,7 @@ export class First extends React.Component {
                     </div>
                     <div className='greeting-flex'>
                         <h1>I am Adrian</h1>
-                        <h2>"Anyone can create but, only the fearless can be great."</h2>
+                        <h2>"Believe you can and you're halfway there."</h2>
                     </div>
                 </div>
             </div>
