1d99a862c83f3ef822c3910df73d566e5e7f195f
Adrian <apparenelle@gmail.com>
2023-05-03 08:12:43 -0400

    fixes
    -resolved grouping issue will deploy this to other modules throughout the page.

--- src/Second.js
+++ src/Second.js
@@ -32,30 +32,30 @@ function Second(props) {
             <div className='second-sectional'>
                 <h2>Passions</h2>
                 <div className='master-collective-passions-flex-container'>
-                    
-                </div>
-                <div className='collective-passions-flex-container'>
-                        <a href='#' className='passion-box-layout'>
-                            <img className='passion-image' src='https://media.giphy.com/media/1569RvwRjejO2IcueA/giphy-downsized-large.gif'></img>
-                            <div className='passion-label'><p>Trading</p></div>
-                        </a>
-                        
-                        <a className='passion-box-layout'href='#'>
-                            <img className='passion-image' src='https://media.giphy.com/media/wwg1suUiTbCY8H8vIA/giphy-downsized-large.gif'></img>
-                            <div className='passion-label'><p>Coding</p></div>
-                        </a>
+                    <div className='collective-passions-flex-container'>
+                            <a href='#' className='passion-box-layout'>
+                                <img className='passion-image' src='https://media.giphy.com/media/1569RvwRjejO2IcueA/giphy-downsized-large.gif'></img>
+                                <div className='passion-label'><p>Trading</p></div>
+                            </a>
+                            
+                            <a className='passion-box-layout'href='#'>
+                                <img className='passion-image' src='https://media.giphy.com/media/wwg1suUiTbCY8H8vIA/giphy-downsized-large.gif'></img>
+                                <div className='passion-label'><p>Coding</p></div>
+                            </a>
 
+                    </div>
+                    <div className='collective-passions-flex-container'>
+                        <a className='passion-box-layout' href='#'>
+                            <img className='passion-image' src='https://media.giphy.com/media/3oEduKi6hUgSVAay4M/giphy.gif'></img>
+                            <div className='passion-label'><p>Engineering</p></div>
+                        </a>
+                        <a className='passion-box-layout' href='#'>
+                            <img className='passion-image' src='https://media.giphy.com/media/26gshn9NXKOMrnysU/giphy.gif'></img>
+                            <div className='passion-label'><p>Photography</p></div>
+                        </a>
+                    </div>
                 </div>
-                <div className='collective-passions-flex-container'>
-                    <a className='passion-box-layout' href='#'>
-                        <img className='passion-image' src='https://media.giphy.com/media/3oEduKi6hUgSVAay4M/giphy.gif'></img>
-                        <div className='passion-label'><p>Engineering</p></div>
-                    </a>
-                    <a className='passion-box-layout' href='#'>
-                        <img className='passion-image' src='https://media.giphy.com/media/26gshn9NXKOMrnysU/giphy.gif'></img>
-                        <div className='passion-label'><p>Photography</p></div>
-                    </a>
-                </div>
+                
             </div>
         );
     }
