3be8ce7e7064e835c4ed04060e8eb441b625247f
Adrian <apparenelle@gmail.com>
2023-05-03 21:50:54 -0400

    tweaks
    tweaks

--- src/Fourth.js
+++ src/Fourth.js
@@ -10,25 +10,28 @@ export class Fourth extends React.Component{
     render(){
         return(
             <div className='fourth-sectional'>
-                <h2>Experiences</h2>
-                <div className='box-layout-flex-row-container'>
-                    {/* You gotta swap the anchors for divs and see if that fixes it */}
-
-                    <div  className='experience-items-flex-container'>
-                        <img className='experience-image' src={nysJobLogo}></img>
-                        <div className='experience-label'><p>State of New York</p></div>
+                <div className='fourth-sectional-container'>
+                    <h2>Experiences</h2>
+                    <div className='box-layout-flex-row-container'>
+                        <div  className='experience-items-flex-container'>
+                            <img className='experience-image' src={nysJobLogo}></img>
+                            <div className='experience-label'><p>State of New York</p></div>
+                        </div>
+                        <div className='experience-items-flex-container'>
+                            <img className='experience-image' src={sculptorJobLogo}></img>
+                            <div className='experience-label'><p>Sculptor Capital Management</p></div>
+                        </div>
+                        
                     </div>
+                    <div className='box-layout-flex-row-container'>
                     <div className='experience-items-flex-container'>
-                        <img className='experience-image' src={sculptorJobLogo}></img>
-                        <div className='experience-label'><p>Sculptor Capital Management</p></div>
-                    </div>
-                    <div className='experience-items-flex-container'>
-                        <img className='experience-image' src={bayernlbJobLogo}></img>
-                        <div className='experience-label'><p>Bayerische LandesBank</p></div>
-                    </div>
-                    <div className='experience-items-flex-container'>
-                        <img className='experience-image' src={anaDataJobLogo}></img>
-                        <div className='experience-label'><p>Ana Data Consulting</p></div>
+                            <img className='experience-image' src={bayernlbJobLogo}></img>
+                            <div className='experience-label'><p>Bayerische LandesBank</p></div>
+                        </div>
+                        <div className='experience-items-flex-container'>
+                            <img className='experience-image' src={anaDataJobLogo}></img>
+                            <div className='experience-label'><p>Ana Data Consulting</p></div>
+                        </div>
                     </div>
                 </div>
             </div>
