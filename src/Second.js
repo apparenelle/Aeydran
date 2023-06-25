58f477b41647b25df188010d4e8eab6b19b70ab6
Adrian <apparenelle@gmail.com>
2023-06-25 01:05:13 -0400

    minor
    some corrections restructuring

--- src/Second.js
+++ src/Second.js
@@ -5,25 +5,30 @@ function Second(props) {
     if(props.isMobile == false) {
         return(
             <section className='second-sectional'>
-                <h2>Passions</h2>
                 <div className='second-sectional-container'>
-                    <div className='collective-passions-flex-container'>
-                        <a href='#' className='passion-box-layout'>
-                            <img className='passion-image' src='https://media.giphy.com/media/1569RvwRjejO2IcueA/giphy-downsized-large.gif'></img>
-                            <div className='passion-label'><p>Trading</p></div>
-                        </a>
-                        <a className='passion-box-layout'href='#'>
-                            <img className='passion-image' src='https://media.giphy.com/media/wwg1suUiTbCY8H8vIA/giphy-downsized-large.gif'></img>
-                            <div className='passion-label'><p>Coding</p></div>
-                        </a>
-                        <a className='passion-box-layout' href='#'>
-                            <img className='passion-image' src='https://media.giphy.com/media/3oEduKi6hUgSVAay4M/giphy.gif'></img>
-                            <div className='passion-label'><p>Engineering</p></div>
-                        </a>
-                        <a className='passion-box-layout' href='#'>
-                            <img className='passion-image' src='https://media.giphy.com/media/26gshn9NXKOMrnysU/giphy.gif'></img>
-                            <div className='passion-label'><p>Photography</p></div>
-                        </a>
+                    <h2>Passions</h2>
+                    <div className='passion-links'>
+                        <div className='collective-passions-flex-container'>
+                            <a href='#' className='passion-box-layout'>
+                                <img className='passion-image' src='https://media.giphy.com/media/1569RvwRjejO2IcueA/giphy-downsized-large.gif'></img>
+                                <div className='passion-label'><p>Trading</p></div>
+                            </a>
+                            <a className='passion-box-layout'href='#'>
+                                <img className='passion-image' src='https://media.giphy.com/media/wwg1suUiTbCY8H8vIA/giphy-downsized-large.gif'></img>
+                                <div className='passion-label'><p>Coding</p></div>
+                            </a>
+                            <a className='passion-box-layout' href='#'>
+                                <img className='passion-image' src='https://media.giphy.com/media/3oEduKi6hUgSVAay4M/giphy.gif'></img>
+                                <div className='passion-label'><p>Engineering</p></div>
+                            </a>
+                            <a className='passion-box-layout' href='#'>
+                                <img className='passion-image' src='https://media.giphy.com/media/26gshn9NXKOMrnysU/giphy.gif'></img>
+                                <div className='passion-label'><p>Photography</p></div>
+                            </a>
+                        </div>
+                        <div className='passion-text'>
+                            Testing Testing  123
+                        </div>
                     </div>
                 </div>
             </section>
