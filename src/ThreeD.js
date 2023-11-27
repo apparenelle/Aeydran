72f77d99c051ad703e11593b8f0c98f6adcbee79
Adrian <apparenelle@gmail.com>
2023-11-27 18:38:56 -0500

    changes

--- src/ThreeD.js
+++ src/ThreeD.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ThreeDStructure from './ThreeDStructure';
-import { Canvas } from '@react-three/fiber';
+import { Canvas,  } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 export default function ThreeD( props ){
     
     return (
         <Canvas>
-        <ambientLight intensity={-4} />
-        <pointLight position={ [10, 10, 10] }/>
-        <ThreeDStructure scale={props.nScale} position = {[0, 0, 0]}/>
+            <OrbitControls/>
+            <ambientLight intensity={-4} />
+            <pointLight position={ [10, 10, 10] }/>
+            <ThreeDStructure scale={props.nScale} position = {[0, 0, 0]}/>
         </Canvas>
     );
 }
